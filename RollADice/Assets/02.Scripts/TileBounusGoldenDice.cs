198ac4cae351b242ed55371e0aee704ff4094e8c
koseyun <a36324606@gmail.com>
2022-10-25 19:50:17 +0900

    RollADice - Concept, Map
    1. Concept
    - Created
    2. Map
    - Made with UI
    - Added some scripts for map tiles

---
+++ RollADice/Assets/02.Scripts/TileBounusGoldenDice.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBounusGoldenDice : Tile
+{
+    public override void OnHere()
+    {
+        base.OnHere();
+        // todo -> Increase golden dice number
+    }
+}
---
+++ RollADice/Assets/02.Scripts/TileBounusNormalDice.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBounusNormalDice : Tile
+{
+    public override void OnHere()
+    {
+        base.OnHere();
+        // todo -> Increase normal dice number
+    }
+}
