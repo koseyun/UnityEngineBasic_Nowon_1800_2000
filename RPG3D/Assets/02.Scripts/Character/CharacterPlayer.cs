f5480157c900a219d4db739f11be731a312ecaec
koseyun <a36324606@gmail.com>
2023-01-13 19:48:29 +0900

    RPG3D - FSM, Character, Terrain
    1. FSM
    - Created IState, CharacterStateBase, CharacterStateMachine and some extensions...
    
    2. Character
    - Created simple base class for character.
    
    3. Terrain
    - Usage of Trees & Detail brushes.

---
+++ RPG3D/Assets/02.Scripts/Character/CharacterPlayer.cs
@@ -0,0 +1,12 @@
+﻿using ULB.RPG.FSM;
+
+namespace ULB.RPG.FSM
+{
+    public class CharacterPlayer : CharacterBase
+    {
+        protected override CharacterStateMachine CreateMachine()
+        {
+            return new PlayerStateMachine(gameObject);
+        }
+    }
+}
---
+++ RPG3D/Assets/02.Scripts/UnityAPIWrappers/AnimatorWrapper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ULB.RPG
+{
+    /// <summary>
+    /// Animator 정보를 래핑하는 클래스
+    /// </summary>
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorWrapper : MonoBehaviour
+    {
+        public bool isPreviousStateFinished { get => true; }
+        public bool isPreviousMachineFinished { get => true; }
+
+        [SerializeField] private Animator _animator;
+
+        public float GetCurrentNormalizedTime(int layer = 0)
+            => _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+}
