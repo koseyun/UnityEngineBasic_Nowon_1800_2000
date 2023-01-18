0020320b9d9510e47470d22a473e88c07e94edf5
koseyun <a36324606@gmail.com>
2023-01-18 19:52:27 +0900

    RPG3D - PlayerStates, KeyInputHandler
    1. PlayerStates
    - Implemented Fall, Land. and tested all states.
    
    2. KeyInputHandler
    - Created to handle actions depends on KeyCode.

--- RPG3D/Assets/02.Scripts/FSM/ExtensionsForPlayer/PlayerStateMachine.cs
+++ RPG3D/Assets/02.Scripts/FSM/ExtensionsForPlayer/PlayerStateMachine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ULB.RPG.InputSysetems;
 using UnityEngine;
 
 namespace ULB.RPG.FSM
@@ -12,6 +13,9 @@ namespace ULB.RPG.FSM
 
         public override void InitStates()
         {
+            Rigidbody rb = owner.GetComponent<Rigidbody>();
+            GroundDetector groundDetector = owner.GetComponent<GroundDetector>();
+
             IState move = new PlayerStateMove(id: (int)StateType.Move,
                                               owner: owner,
                                               canExecute: () => true,
@@ -25,6 +29,55 @@ namespace ULB.RPG.FSM
                                               },
                                               hasExitTime: false);
             states.Add(StateType.Move, move);
+
+            IState jump = new PlayerStateJump(id: (int)StateType.Jump,
+                                              owner: owner,
+                                              canExecute: () => groundDetector.isDetected,
+                                              transitions: new List<KeyValuePair<Func<bool>, int>>()
+                                              {
+                                                  new KeyValuePair<Func<bool>, int>
+                                                  (
+                                                      () => rb.velocity.y <= 0 && groundDetector.isDetected,
+                                                      (int)StateType.Land
+                                                  ),
+                                                  new KeyValuePair<Func<bool>, int>
+                                                  (
+                                                      () => rb.velocity.y <= 0, //바꿀 조건
+                                                      (int)StateType.Fall //바꿀 상태
+                                                  ),
+                                              },
+                                              hasExitTime: false);
+            states.Add(StateType.Jump, jump);
+
+            IState fall = new PlayerStateFall(id: (int)StateType.Fall,
+                                              owner: owner,
+                                              canExecute: () => groundDetector.isDetected == false,
+                                              transitions: new List<KeyValuePair<Func<bool>, int>>()
+                                              {
+                                                  new KeyValuePair<Func<bool>, int>
+                                                  (
+                                                      () => groundDetector.isDetected,
+                                                      (int)StateType.Land
+                                                  ),
+                                              },
+                                              hasExitTime: false);
+            states.Add(StateType.Fall, fall);
+
+            IState land = new PlayerStateLand(id: (int)StateType.Land,
+                                              owner: owner,
+                                              canExecute: () => groundDetector.isDetected,
+                                              transitions: new List<KeyValuePair<Func<bool>, int>>()
+                                              {
+                                                  new KeyValuePair<Func<bool>, int>
+                                                  (
+                                                      () => true,
+                                                      (int)StateType.Fall
+                                                  ),
+                                              },
+                                              hasExitTime: true);
+            states.Add(StateType.Land, land);
+
+            KeyInputHandler.instance.RegisterKeyPressAction(KeyCode.Space, () => ChangeState(StateType.Jump));
         }
     }
 }
