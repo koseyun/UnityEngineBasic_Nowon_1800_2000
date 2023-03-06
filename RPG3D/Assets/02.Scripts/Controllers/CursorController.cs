6972656be9a39c7f9871ab866d954cf247de583b
koseyun <a36324606@gmail.com>
2023-03-06 19:45:41 +0900

    RPG3D - CursorController
    1. CursorController
    - Block user input to control player ( KeyInputHandler & CameraController ) when cursor is activated.
    - Deactive cursor when user click a GameObject excludes UIs.
    
    2. StandaloneInputModuleWrapper
    - Derived to judge GameObject with specific casting modules.

--- RPG3D/Assets/02.Scripts/Controllers/CursorController.cs
+++ RPG3D/Assets/02.Scripts/Controllers/CursorController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using ULB.RPG.InputSystems;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CursorController : SingletonMonoBase<CursorController>
 {
@@ -8,19 +11,38 @@ public class CursorController : SingletonMonoBase<CursorController>
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
+        KeyInputHandler.instance.SetActive(false);
+        CameraController.instance.SetActive(false);
     }
 
     public void DeactiveCursor()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        KeyInputHandler.instance.SetActive(true);
+        CameraController.instance.SetActive(true);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Cursor.visible &&
+            Input.GetMouseButtonDown(0) &&
+            StandaloneInputModuleWrapper.main.IsPointerOverGameObject<GraphicRaycaster>(StandaloneInputModule.kMouseLeftId) == false)
+        {
+            DeactiveCursor();
+        }
+        else if (Cursor.visible == false && 
+                 Input.GetKeyDown(KeyCode.BackQuote))//BackQuote -> `
         {
             ActiveCursor();
         }
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, float.PositiveInfinity, LayerMask.NameToLayer("UI")))
+            {
+            }
+        }*/
     }
 }
